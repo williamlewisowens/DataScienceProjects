09813c895a0ca60133f2ca73446b75471a006e68
wowens <williamlewisowens@gmail.com>
2018-03-01 23:20:02 -0600

    Initial commit

---
+++ Tweet_Analysis/credentials.py
@@ -0,0 +1,9 @@
+# Twitter App access keys for @user
+
+# Consume:
+CONSUMER_KEY = 'bVlX0w8aHBJBCgwDYwTy97CdX'
+CONSUMER_SECRET = 'KXhzIyOKSg05UK2WC3QzMhMYtoqeaUYI6SjmZxKgufVbcDB347'
+
+# Access:
+ACCESS_TOKEN = '278590711-ayL85DLmwq0d9uU1U6tFP1mkl0v9XuKU3GlrftK5'
+ACCESS_SECRET = 'Gv8ySyGM4x6aIAgNdD5jb0b5cyKXHtHypQiTfkiDPE7B3'
